fd33d63123ea114f0357221363677d5af533100d
Qadriga <rob.sandhausen@freenet.de>
2018-05-10 13:05:56 +0200

    added popup windows for when new port is connected

---
+++ GUI_Starter/popups/NewPort.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI_Starter.popups
+{
+    /// <summary>
+    /// Interaktionslogik für NewPort.xaml
+    /// </summary>
+    public partial class NewPort : Window
+    {
+        public NewPort()
+        {
+            InitializeComponent();
+        }
+        
+        public void SetMessageText(String Message)
+        {
+            this.InfoBlock.Text = Message;
+        }
+        private void onBtn_Oklick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
